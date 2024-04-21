f79b5fd895e9d60937626e8d44186f8f49ed4b0c
Coding in Flow <info@codinginflow.com>
2024-04-21 12:14:28 +0200

    End of part 7

--- src/app/settings/actions.ts
+++ src/app/settings/actions.ts
@@ -1,13 +1,30 @@
 "use server";
 
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
 import { UpdateProfileValues, updateProfileSchema } from "@/lib/validation";
+import { revalidatePath } from "next/cache";
 
 // To learn more about server actions, watch my YT tutorial: https://www.youtube.com/watch?v=XD5FpbVpWzk
 
 export async function updateProfile(values: UpdateProfileValues) {
-  // TODO: Get the currently authenticated user
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) {
+    throw Error("Unauthorized");
+  }
 
   const { name } = updateProfileSchema.parse(values);
 
-  // TODO: Update user
+  await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      name,
+    },
+  });
+
+  revalidatePath("/");
 }
