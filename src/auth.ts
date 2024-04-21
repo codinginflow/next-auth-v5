f3d84a75f0d39efc7a1143e9668a13392b365200
Coding in Flow <info@codinginflow.com>
2024-04-21 11:03:29 +0200

    End of part 4

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,13 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import prisma from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme: {
+    logo: "/logo.png",
+  },
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Google, GitHub],
 });
