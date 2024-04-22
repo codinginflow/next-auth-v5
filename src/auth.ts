547ae5dfbbae65c6cb3f5aec4fb8427db02af4ea
Coding in Flow <info@codinginflow.com>
2024-04-22 10:20:32 +0200

    End of part 11

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import Resend from "next-auth/providers/resend";
 import prisma from "./lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -17,5 +18,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return session;
     },
   },
-  providers: [Google, GitHub],
+  providers: [
+    Google,
+    GitHub,
+    Resend({
+      from: "no-reply@tutorial.codinginflow.com",
+    }),
+  ],
 });
