689176b38e71d09b5bde3d10ce9a65892e7401b0
Joseph Cutor <cutormarkjoseph@gmail.com>
2024-05-26 20:28:12 +0800

    default

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ["avatars.githubusercontent.com"],
     remotePatterns: [
       {
-        hostname: "lh3.googleusercontent.com",
+        hostname: "lh3.googleusercontent.com", 
+        //it is better not to include it here. it has payment. 
+        //it is better if you will let user upload their profile pictures
       },
     ],
   },
--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,11 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import Resend from "next-auth/providers/resend";
 import prisma from "./lib/prisma";
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
@@ -26,3 +27,9 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
 });
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   adapter: PrismaAdapter(prisma) as Adapter,
+//   providers: [],
+// });
+
